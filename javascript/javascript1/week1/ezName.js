1492b73c6a461394a61ddf4a2ffa436e7bccec4c
AnilMula <anilreddymula@gmail.com>
2020-05-12 12:39:18 +0200

    fourth script added

---
+++ javascript/javascript1/week1/ezName.js
@@ -0,0 +1,9 @@
+
+let firstWords = ["Easy", "Awesome", "Wonderful","Ideal","Professional","Creative","Hub","Solution"];
+let secondWords = ["Feasible", "Valuable", "Pricy","Incredible","Hoody","Easy", "Awesome", "Wonderful"];
+const num1 = 2;
+const num2 = 2;
+let startupName = firstWords[num1] + ' ' + secondWords[num2];
+console.log(" The startup: " +  " \" " + startupName  + " \" " + " contains " + startupName.length + " characters");
+  
+       
