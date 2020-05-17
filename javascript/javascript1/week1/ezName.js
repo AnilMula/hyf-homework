24ec0ed4f1fe1a984570fda57aea9ee30768e443
AnilMula <anilreddymula@gmail.com>
2020-05-17 08:59:14 +0200

    used random number generator function to generate a rondom number

--- javascript/javascript1/week1/ezName.js
+++ javascript/javascript1/week1/ezName.js
@@ -1,8 +1,8 @@
 
 let firstWords = ["Easy", "Awesome", "Wonderful","Ideal","Professional","Creative","Hub","Solution"];
 let secondWords = ["Feasible", "Valuable", "Pricy","Incredible","Hoody","Easy", "Awesome", "Wonderful"];
-const num1 = 2;
-const num2 = 2;
+const num1 = Math.floor(Math.random() * 10) + 0;
+const num2 = Math.floor(Math.random() * 10) + 0;
 let startupName = firstWords[num1] + ' ' + secondWords[num2];
 console.log(" The startup: " +  " \" " + startupName  + " \" " + " contains " + startupName.length + " characters");
   
