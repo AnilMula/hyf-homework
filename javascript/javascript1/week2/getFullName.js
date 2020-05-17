21c47c21456e062c4f37da017c3469b0c43afc85
AnilMula <anilreddymula@gmail.com>
2020-05-17 20:21:41 +0200

    changed data type from var to const

--- javascript/javascript1/week2/getFullName.js
+++ javascript/javascript1/week2/getFullName.js
@@ -7,7 +7,7 @@ function getFullName(firstname, surname, useFormalName){
     }
 }
 
-var fullname1 = getFullName("Anil","Mula",false);
-var fullname2 = getFullName("Kristen", "Threshow", true);
+const fullname1 = getFullName("Anil","Mula",false);
+const fullname2 = getFullName("Kristen", "Threshow", true);
 console.log(fullname1);
 console.log(fullname2);
