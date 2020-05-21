a2563eb1e0d882ccef4c5ed4268449ce67cca940
AnilMula <60918335+AnilMula@users.noreply.github.com>
2020-05-21 12:20:07 +0200

    Add files via upload
    Get Event Week day interactive program added

---
+++ javascript/javascript1/week2/Event Week Day/EventWeekDay.js
@@ -0,0 +1,11 @@
+function getEventWeekday(){
+    /* The purpose of this function is to return a weekday 
+    that is number of days from today */
+    // days is string array that holds all the week days 
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const d = new Date(); // d is an instance of an  Date object
+    let today = d.getDay(); // getday() method returns a number 0=sunday...6=saturday
+    today += document.getElementById("numdays").value;
+    
+    document.getElementById("weekday").innerHTML = "The event is on "+days[today%7-1];  //return a weekday that is number of days from today 
+}
