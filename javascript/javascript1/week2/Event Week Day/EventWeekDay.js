61dd51d42b550e10f1670486a2528cf1be448091
AnilMula <anilreddymula@gmail.com>
2020-05-26 11:49:28 +0200

    modified javascript

--- javascript/javascript1/week2/Event Week Day/EventWeekDay.js
+++ javascript/javascript1/week2/Event Week Day/EventWeekDay.js
@@ -1,11 +1,20 @@
-function getEventWeekday(){
-    /* The purpose of this function is to return a weekday 
+function getEventWeekday() {
+  /* The purpose of this function is to return a weekday 
     that is number of days from today */
-    // days is string array that holds all the week days 
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const d = new Date(); // d is an instance of an  Date object
-    let today = parseInt(d.getDay()); // getday() method returns a number 0=sunday...6=saturday
-    today += document.getElementById("numdays").value;
-    
-    document.getElementById("weekday").innerHTML = "today is "+today+" The event is on "+days[today%7];  //return a weekday that is number of days from today 
+  // days is string array that holds all the week days
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const d = new Date(); // d is an instance of an  Date object
+  let today = d.getDay(); // getday() method returns a number 0=sunday...6=saturday
+  today += parseInt(document.getElementById("numdays").value);
+
+  document.getElementById("weekday").innerHTML =
+    " The event is on " + days[today % 7]; //return a weekday that is number of days from today
 }
