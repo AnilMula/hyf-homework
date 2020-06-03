836cd819bf1856c68bdede9ca8e5d1f6489ea369
AnilMula <anilreddymula@gmail.com>
2020-06-03 15:28:13 +0200

    added object to hold bought cany details, and improved output

--- javascript/javascript1/week2/Candy Helper/CandyHelper.js
+++ javascript/javascript1/week2/Candy Helper/CandyHelper.js
@@ -16,24 +16,46 @@ function buyCandy() {
     let weight = parseInt(document.getElementById("weight").value);
 
     if (canBuyMoreCandy(candyType, weight)) {
+      document.getElementById("output").style.color = "black";
       document.getElementById("output").innerHTML =
         '"You can buy more, so please do!"';
-      addCandy(candyType, weight);
-      document.getElementById("output1").innerHTML = boughtCandyPrices;
+      addCandy(candyType, weight, costOfCandy);
+
+      document.getElementById("output1").innerHTML =
+        "Remaining Amount " + amountToSpend;
+      let outputMessage = " ";
+      for (let i = 0; i < boughtCandyPrices.length; i++) {
+        outputMessage +=
+          boughtCandyPrices[i].candyType +
+          " " +
+          boughtCandyPrices[i].weight +
+          "grams and cost:" +
+          boughtCandyPrices[i].price +
+          "<br>";
+      }
+      document.getElementById("output2").innerHTML = outputMessage;
     } else {
+      document.getElementById("output").style.color = "red";
       document.getElementById("output").innerHTML = "Enough candy for you!";
     }
   } else {
+    document.getElementById("output").style.color = "red";
     document.getElementById("output").innerHTML = "Enough candy for you!";
   }
 }
 
-function addCandy(candyType, weight) {
-  boughtCandyPrices.push(candyType, weight); // push candy and cost of candy into an array
+function addCandy(candyType, weight, price) {
+  boughtCandyPrices.push({ candyType, weight, price }); // push candy and cost of candy into an array
   amountToSpend -= costOfCandy; //since candy we reserved: reduce the amount to spend
 }
 
 function canBuyMoreCandy(candyType, weight) {
+  /*   // the following code is display the remaining amount
+  const initializeDiv = document.getElementById("initialize");
+  const innerDiv = document.createElement("div");
+
+  innerDiv.innerHTML = "remaining amount<br>" + amountToSpend;
+  initializeDiv.appendChild(innerDiv); */
   // the function will check if we have more money to buy extra candies
   for (let i = 0; i < candyTablePrices.length; i++) {
     // iterate through candy prices table to calculate price of a candy
