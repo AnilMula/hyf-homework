12cdd6d019541ff7665f2fd568e5d39d616d4a53
AnilMula <anilreddymula@gmail.com>
2020-06-17 01:03:31 +0200

    added a functionlity to sort products based on product name, cheap items first, and expensive items

--- javascript/javascript2/week2/hyfBay/main.js
+++ javascript/javascript2/week2/hyfBay/main.js
@@ -37,6 +37,47 @@ inputSearch.addEventListener("input", () => {
     product.name.includes(inputSearch.value)
   );
   const productsUl = document.querySelector("section.products ul");
-  console.log((productsUl.innerHTML = " "));
+  productsUl.innerHTML = " ";
   renderProducts(sortedProducts);
+});
+
+/* 
+task2 : to sort the products based on prices like
+expensive first or cheap first */
+
+//add "..change-event.."  to the select field
+const priceSearch = document.getElementById("sort-price-wise");
+
+priceSearch.addEventListener("change", function () {
+  if (priceSearch.value === "cheap") {
+    // logic to sort the object based on price...cheap first
+    const sortedProducts = products.sort((a, b) =>
+      a.price > b.price ? 1 : b.price > a.price ? -1 : 0
+    );
+
+    //format output on the Document
+    const productsUl = document.querySelector("section.products ul");
+    productsUl.innerHTML = " ";
+    renderProducts(sortedProducts);
+  } else if (priceSearch.value === "expensive") {
+    // logic to sort the object based on price...expensive first
+    const sortedProducts = products.sort((a, b) =>
+      a.price < b.price ? 0 : b.price < a.price ? -1 : 1
+    );
+
+    //format output on the Document
+    const productsUl = document.querySelector("section.products ul");
+    productsUl.innerHTML = " ";
+    renderProducts(sortedProducts);
+  } else if (priceSearch.value === "name") {
+    // logic to sort the object based on name
+    const sortedProducts = products.sort((a, b) =>
+      a.name > b.name ? 1 : b.name > a.name ? -1 : 0
+    );
+
+    //format output on the Document
+    const productsUl = document.querySelector("section.products ul");
+    productsUl.innerHTML = " ";
+    renderProducts(sortedProducts);
+  }
 });
