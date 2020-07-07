ddddb32ff7ba9cb5a48230eab4677bd4628b4f6a
AnilMula <anilreddymula@gmail.com>
2020-07-07 13:01:07 +0200

    visual promise added

---
+++ javascript/javascript3/week2/Visual Promise/main.js
@@ -0,0 +1,30 @@
+const targetPositions = [
+  { x: 20, y: 300 },
+  { x: 400, y: 300 },
+  { x: 400, y: 20 },
+];
+
+function translateOneByOne() {
+  moveElement(redBox, { x: 20, y: 300 }).then(() => {
+    console.log("Element has been moved");
+
+    moveElement(blueBox, { x: 400, y: 300 }).then(() => {
+      console.log("Element has been moved");
+
+      moveElement(greenBox, { x: 400, y: 20 }).then(() => {
+        console.log("Element has been moved");
+      });
+    });
+  });
+}
+
+translateOneByOne();
+function translateAllAtOnce() {
+  const redBoxPromise = moveElement(redBox, { x: 20, y: 300 });
+  const blueBoxPromise = moveElement(blueBox, { x: 400, y: 300 });
+  const greenBoxPromise = moveElement(greenBox, { x: 400, y: 20 });
+  Promise.all([redBoxPromise, blueBoxPromise, greenBoxPromise]).then(() => {
+    console.log("all the elements have been moved");
+  });
+}
+//translateAllAtOnce();
