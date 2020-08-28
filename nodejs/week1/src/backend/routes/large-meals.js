c90c272c85c1e299ac4d72990a16f5a8caed9283
AnilMula <anilreddymula@gmail.com>
2020-08-28 13:17:09 +0200

    updated latrgeMeals.js route to include reviews and changed max_reservations to 11 in meals.json data

--- nodejs/week1/src/backend/routes/large-meals.js
+++ nodejs/week1/src/backend/routes/large-meals.js
@@ -2,9 +2,29 @@ const express = require("express");
 const app = express();
 
 const mealsData = require("../data/meals.json");
+const reviewData = require("../data/reviews.json");
 
 // a meal is large if it can serve more people fx.10 or more
 const largeMeal = mealsData.filter((meal) => meal.max_reservations > 10);
+
+//compare two json files to add reviews that matches a meal id
+largeMeal.forEach((meal) => {
+  //iterate over meals data
+  const matchedReviewData = []; // a temporary array to hold review data when there is a match
+  reviewData.forEach((review) => {
+    //iterate over reviews data
+    if (meal.id == review.meal_id) {
+      //if the meal has a review
+      matchedReviewData.push(review);
+    }
+    if (matchedReviewData.length == 0) {
+      // if there are no reviews for a meal
+      meal["reviews"] = ["NO reviews Yet"];
+    } else {
+      meal["reviews"] = matchedReviewData;
+    }
+  });
+});
 
 app.get("/largeMeal", function (request, response) {
   response.send(largeMeal);
