f630a2f17d32898ac5e8f59f9ba3f72033961fda
AnilMula <anilreddymula@gmail.com>
2020-08-28 13:05:16 +0200

    updated meals.js route to include reviews

--- nodejs/week1/src/backend/routes/meals.js
+++ nodejs/week1/src/backend/routes/meals.js
@@ -2,7 +2,26 @@ const express = require("express");
 const app = express();
 
 const mealsData = require("../data/meals.json");
-//const meals = JSON.parse(mealsData);
+const reviewData = require("../data/reviews.json");
+
+//compare two json files to add reviews that matches a meal id
+mealsData.forEach((meal) => {
+  //iterate over meals data
+  const matchedReviewData = []; // a temporary array to hold review data when there is a match
+  reviewData.forEach((review) => {
+    //iterate over reviews data
+    if (meal.id == review.meal_id) {
+      //if the meal has a review
+      matchedReviewData.push(review);
+    }
+    if (matchedReviewData.length == 0) {
+      // if there are no reviews for a meal
+      meal["reviews"] = ["NO reviews Yet"];
+    } else {
+      meal["reviews"] = matchedReviewData;
+    }
+  });
+});
 
 app.get("/meals", function (request, response) {
   response.send(mealsData);
