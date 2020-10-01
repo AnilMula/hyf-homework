8956ee1132c34f99e3539adbf630b913f02a9e76
AnilMula <anilreddymula@gmail.com>
2020-10-01 05:40:51 +0200

    used map to reduce the components

--- react/week1/ToDo.js
+++ react/week1/ToDo.js
@@ -1,10 +1,8 @@
 import React from 'react';
+
 import ToDo1 from './ToDo1';
-import ToDo2 from './ToDo2';
-import ToDo3 from './ToDo3';
 
 const ToDo = () => {
-  //few styles for todo
   const boxStyle = {
     color: 'green',
     border: '2px solid black',
@@ -14,34 +12,35 @@ const ToDo = () => {
   const underline = {
     textDecoration: 'underline',
   };
-  const todo1Color = {
-    color: 'black',
-    backgroundColor: 'LightBlue',
-  };
-  const todo2Color = {
-    color: 'red',
-    backgroundColor: 'LightBlue',
-  };
-
-  const todo3Color = {
-    color: 'purple',
-    backgroundColor: 'LightBlue',
-  };
+  const todoStyles = [
+    {
+      color: 'black',
+      backgroundColor: 'LightBlue',
+    },
+    {
+      color: 'red',
+      backgroundColor: 'LightBlue',
+    },
+    {
+      color: 'purple',
+      backgroundColor: 'LightBlue',
+    },
+  ];
+  const tasks = [
+    { activity: 'get out of bed', time: '06:00' },
+    { activity: 'Brush teeth', time: '06:15' },
+    { activity: 'Eat breakfast', time: '06:55' },
+  ];
   return (
     <div style={boxStyle}>
       <h1 style={underline}>Todo List</h1>
-      <ToDo1
-        task={{ activity: 'get out of bed', time: '06:00' }}
-        style={todo1Color}
-      ></ToDo1>
-      <ToDo2
-        task={{ activity: 'Brush teeth', time: '06:15' }}
-        style={todo2Color}
-      ></ToDo2>
-      <ToDo3
-        task={{ activity: 'Eat breakfast', time: '06:55' }}
-        style={todo3Color}
-      ></ToDo3>
+      {tasks
+        .map((task, index) => [task, todoStyles[index]])
+        .map((styledTask, index) => (
+          <div key={index}>
+            <ToDo1 task={styledTask[0]} style={styledTask[1]}></ToDo1>
+          </div>
+        ))}
     </div>
   );
 };
