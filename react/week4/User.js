fcf5a4e96b53c03041ae733b20ef29bcfa3f2930
AnilMula <anilreddymula@gmail.com>
2020-10-23 05:19:21 +0200

    Git hub user search implemented

--- react/week4/User.js
+++ react/week4/User.js
@@ -1,13 +1,9 @@
 import React, { useState } from 'react';
 
 function User({ user }) {
-  const [showRepos, setShowRepos] = useState(false);
-  const handleMouseOver = (e) => {
-    setShowRepos(true);
-  };
   return (
     <div>
-      <div onMouseOver={handleMouseOver}>{user.login}</div>
+      <div>{user.login}</div>
     </div>
   );
 }
