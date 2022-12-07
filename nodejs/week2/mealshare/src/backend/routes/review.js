013403c695055ef49fe4f88e95ac453d29276204
AnilMula <60918335+AnilMula@users.noreply.github.com>
2022-12-07 05:44:07 +0100

    Delete nodejs/week2 directory

--- nodejs/week2/mealshare/src/backend/routes/review.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const reviewsData = require("../data/reviews.json");
-
-router.get("/reviews", function (request, response) {
-  response.send(reviewsData);
-});
-
-module.exports = router;
--- nodejs/week2/mealshare/src/backend/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app");
-
-const server = app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
-});
-
-module.exports = server;
